46de9fe84fb66812792b71b752be01327f5d19be
YulliaBerezniak <fedorova2097@gmail.com>
2024-09-30 22:45:09 +0300

    Changed image url

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -13,7 +13,7 @@ const App = () => {
     return (
       <div>
         <Card
-          imageUrl="https://www.pexels.com/photo/photo-of-coconhttps://images.pexels.com/photos/1576955/pexels-photo-1576955.jpegt-tree-on-seashore-1576955/"
+          imageUrl="https://images.pexels.com/photos/1576955/pexels-photo-1576955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           title="Coconut Tree"
           description="This is a photo of Coconut Tree On Seashore in Fort Lauderdale, FL, United State."
         />
